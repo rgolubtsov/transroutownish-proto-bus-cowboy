6a18f788a4a6cc018025ffdcf8f0326bf7835b45
Radislav (Radicchio) Golubtsov <rgolubtsov@mail.ru>
2022-11-30 17:45:00 +0300

    Helper: (1) Add common error messages. (2) Add constants regarding the server port number. (3) Retrieve safely the port number used to run the server. (4) Fix retrieving safely the debug logging enablement setting.

--- apps/bus/src/bus_helper.hrl
+++ apps/bus/src/bus_helper.hrl
@@ -25,23 +25,63 @@
 -define(V_BAR,         "|").
 -define(NEW_LINE,     "\n").
 
+% Common error messages.
+-define(ERR_PORT_VALID_MUST_BE_POSITIVE_INT,
+        "Valid server port must be a positive integer value, "
+        "in the range 1024 .. 49151. The default value of 8080 "
+        "will be used instead.").
+-define(ERR_DATASTORE_NOT_FOUND,
+        "FATAL: Data store file not found. Quitting...").
+
 % Common notification messages.
 -define(MSG_WORK_IN_PROGRESS, "This is a work in progress"
                         " - " "please wait for a while...").
+
+%% ----------------------------------------------------------------------------
+%% @doc The minimum port number allowed.
+-define(MIN_PORT, 1024).
+
+%% ----------------------------------------------------------------------------
+%% @doc The maximum port number allowed.
+-define(MAX_PORT, 49151).
+
+%% ----------------------------------------------------------------------------
+%% @doc The default server port number.
+-define(DEF_PORT, 8080).
 
 % -----------------------------------------------------------------------------
 % Helper function. Used to get the application settings.
 %
 % Returns: The tuple containing values of individual settings.
 get_settings() ->
+    % Retrieving the port number used to run the server.
+    ServerPort_ = application:get_env(server_port),
+
+    ServerPort  = integer_to_list(if (ServerPort_ =/= undefined) ->
+        ServerPort__ = element(2, ServerPort_),
+
+        if ((ServerPort__ >= ?MIN_PORT)
+        and (ServerPort__ =< ?MAX_PORT)) -> ServerPort__;
+           (true) ->
+            io:put_chars(?ERR_PORT_VALID_MUST_BE_POSITIVE_INT), io:nl(),
+
+            ?DEF_PORT
+        end;
+       (true) ->
+        io:put_chars(?ERR_PORT_VALID_MUST_BE_POSITIVE_INT), io:nl(),
+
+        ?DEF_PORT
+    end),
+
+    % Identifying, whether debug logging is enabled.
     DebugLogEnabled_ = application:get_env(logger_debug_enabled),
 
-    DebugLogEnabled  = if (DebugLogEnabled_ =/= undefined) ->
-        if (element(2, DebugLogEnabled_) =:= yes) -> true; (true) -> false end;
-                                                           (true) -> false end,
+    DebugLogEnabled  = atom_to_list(if (DebugLogEnabled_ =/= undefined) ->
+        if (element(2, DebugLogEnabled_) =:= yes) -> true;
+           (true) -> false end; (true) -> false end),
 
     {
-        element(2, application:get_env(server_port)),
+        ServerPort,
         DebugLogEnabled, % <== "true" or "false".
 
         % The path and filename of the routes data store (as 3rd tuple elem).
